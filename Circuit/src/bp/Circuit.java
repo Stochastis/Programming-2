c7484123bebeff3942595744683c874aceeac958
Caleb <214475@student.cofo.edu>
2020-01-19 22:43:59 -0600

    Step 1 complete.

--- Circuit/src/bp/Circuit.java
+++ Circuit/src/bp/Circuit.java
@@ -4,70 +4,15 @@
 package bp;
 
 /**
- * @author cross
+ * @author Caleb
  *
  */
-public class Circuit implements ICircuit {
-	private double voltage;
-	private double amperage;
-	private double resistance;
+public class Circuit {
 
-	@Override
-	public double getVoltage() {
-		// TODO Auto-generated method stub
-		return voltage;
-	}
-
-	@Override
-	public double getAmperage() {
-		// TODO Auto-generated method stub
-		return amperage;
-	}
-
-	@Override
-	public double getResistance() {
-		// TODO Auto-generated method stub
-		return resistance;
-	}
-
-	@Override
-	public void setVoltage(double pVoltage) {
-		voltage = pVoltage;
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setAmperage(double pAmperage) {
-		amperage = pAmperage;
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setResistance(double pResistance) {
-		resistance = pResistance;
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void calculateVoltage() {
-		voltage = amperage * resistance;
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void calculateAmperage() {
-		amperage = voltage / resistance;
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void calculateResistance() {
-		resistance = voltage / amperage;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
