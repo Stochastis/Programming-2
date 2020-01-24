5037344e9596a5d9f41a4607a6aef05d553cdf35
214475 <214475@PLSLAB20760.hardworku.cofo.edu>
2020-01-24 11:47:08 -0600

    Jan. 24, 2020 at 11:46. End of class commit. User interface working.
    Still need to implement the functionality of the clear button.

--- CircuitFX/src/application/Main.java
+++ CircuitFX/src/application/Main.java
@@ -6,9 +6,14 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+/**
+ * 
+ * @author 214475
+ *
+ */
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public final void start(final Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/view/View.fxml"));
 			Scene scene = new Scene(root);
@@ -21,7 +26,11 @@ public class Main extends Application {
 		}
 	}
 
-	public static void main(String[] args) {
+	/**
+	 * 
+	 * @param args These arguments will not be used.
+	 */
+	public static void main(final String[] args) {
 		launch(args);
 	}
 }
--- CircuitFX/src/controller/Controller.java
+++ CircuitFX/src/controller/Controller.java
@@ -4,15 +4,80 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import model.Circuit;
 
+/**
+ * 
+ * @author 214475
+ *
+ */
 public class Controller {
 
+	/**
+	 * 
+	 */
 	@FXML
-	void handleClick(ActionEvent event) {
-		Alert myAlert = new Alert(AlertType.INFORMATION);
-		myAlert.setTitle("Hello");
-		myAlert.setContentText("I have the high ground.");
-		myAlert.showAndWait();
+	private TextField amperageTextField;
+
+	/**
+	 * 
+	 */
+	@FXML
+	private TextField resistanceTextField;
+
+	/**
+	 * 
+	 */
+	@FXML
+	private TextField voltageTextField;
+
+	@FXML
+	final void handleClick(final ActionEvent event) {
+
+		Circuit myCircuit = new Circuit();
+
+		if ((amperageTextField.getText().isBlank())
+				&& (!(resistanceTextField.getText().isBlank()) && (!(voltageTextField.getText().isBlank())))) {
+			myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
+			myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
+			myCircuit.calculateAmperage();
+
+			Alert myAlert = new Alert(AlertType.INFORMATION);
+			myAlert.setTitle("Results");
+			myAlert.setContentText("This circuit has an amperage of " + myCircuit.getAmperage());
+			myAlert.showAndWait();
+		} else if ((resistanceTextField.getText().isBlank())
+				&& (!(amperageTextField.getText().isBlank()) && (!(voltageTextField.getText().isBlank())))) {
+			myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
+			myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
+			myCircuit.calculateResistance();
+
+			Alert myAlert = new Alert(AlertType.INFORMATION);
+			myAlert.setTitle("Results");
+			myAlert.setContentText("This circuit has a resistance of " + myCircuit.getResistance());
+			myAlert.showAndWait();
+		} else if ((voltageTextField.getText().isBlank())
+				&& (!(amperageTextField.getText().isBlank()) && (!(resistanceTextField.getText().isBlank())))) {
+			myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
+			myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
+			myCircuit.calculateVoltage();
+
+			Alert myAlert = new Alert(AlertType.INFORMATION);
+			myAlert.setTitle("Results");
+			myAlert.setContentText("This circuit has a voltage of " + myCircuit.getVoltage());
+			myAlert.showAndWait();
+		} else {
+			Alert myAlert = new Alert(AlertType.INFORMATION);
+			myAlert.setTitle("Error");
+			myAlert.setContentText("Please enter values in two of the three boxes.");
+			myAlert.showAndWait();
+		}
+
 	}
 
+	@FXML
+	void handleClear(ActionEvent event) {
+
+	}
 }
