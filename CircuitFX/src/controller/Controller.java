9222dabc65cdf56672c9dab4f4aa14b598c87aff
Caleb <214475@student.cofo.edu>
2020-01-29 00:41:48 -0600

    Jan. 29, 2020 @ 0:41. Working.
    Program functioning as expected.

--- CircuitFX/src/controller/Controller.java
+++ CircuitFX/src/controller/Controller.java
@@ -32,42 +32,102 @@ public class Controller {
 	@FXML
 	private TextField voltageTextField;
 
-	Circuit myCircuit = new Circuit();
+	/**
+	 * 
+	 */
+	private Circuit myCircuit = new Circuit();
 
 	@FXML
 	final void handleClick(final ActionEvent event) {
 
 		if ((amperageTextField.getText().isBlank())
 				&& (!(resistanceTextField.getText().isBlank()) && (!(voltageTextField.getText().isBlank())))) {
+			boolean error = true;
 
-			myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
-			myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
-			myCircuit.calculateAmperage();
+			while (!error) {
+				try {
+					myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
+					myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
+					if ((myCircuit.getResistance() <= 0) || (myCircuit.getVoltage() <= 0)) {
+						Alert myAlert = new Alert(AlertType.INFORMATION);
+						myAlert.setTitle("Error");
+						myAlert.setContentText("Please enter positive values.");
+						myAlert.showAndWait();
+						break;
+					}
+					myCircuit.calculateAmperage();
+					error = false;
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Results");
+					myAlert.setContentText("This circuit has an amperage of " + myCircuit.getAmperage());
+					myAlert.showAndWait();
+				} catch (Exception e) {
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Error");
+					myAlert.setContentText("Please make sure that the values you entered are valid.");
+					myAlert.showAndWait();
+					break;
+				}
+			}
 
-			Alert myAlert = new Alert(AlertType.INFORMATION);
-			myAlert.setTitle("Results");
-			myAlert.setContentText("This circuit has an amperage of " + myCircuit.getAmperage());
-			myAlert.showAndWait();
 		} else if ((resistanceTextField.getText().isBlank())
 				&& (!(amperageTextField.getText().isBlank()) && (!(voltageTextField.getText().isBlank())))) {
-			myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
-			myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
-			myCircuit.calculateResistance();
+			boolean error = true;
 
-			Alert myAlert = new Alert(AlertType.INFORMATION);
-			myAlert.setTitle("Results");
-			myAlert.setContentText("This circuit has a resistance of " + myCircuit.getResistance());
-			myAlert.showAndWait();
+			while (!error) {
+				try {
+					myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
+					myCircuit.setVoltage(Double.parseDouble(voltageTextField.getText()));
+					if ((myCircuit.getAmperage() <= 0) || (myCircuit.getVoltage() <= 0)) {
+						Alert myAlert = new Alert(AlertType.INFORMATION);
+						myAlert.setTitle("Error");
+						myAlert.setContentText("Please enter positive values.");
+						myAlert.showAndWait();
+						break;
+					}
+					myCircuit.calculateResistance();
+					error = false;
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Results");
+					myAlert.setContentText("This circuit has an resistance of " + myCircuit.getResistance());
+					myAlert.showAndWait();
+				} catch (Exception e) {
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Error");
+					myAlert.setContentText("Please make sure that the values you entered are valid.");
+					myAlert.showAndWait();
+					break;
+				}
+			}
 		} else if ((voltageTextField.getText().isBlank())
 				&& (!(amperageTextField.getText().isBlank()) && (!(resistanceTextField.getText().isBlank())))) {
-			myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
-			myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
-			myCircuit.calculateVoltage();
+			boolean error = true;
 
-			Alert myAlert = new Alert(AlertType.INFORMATION);
-			myAlert.setTitle("Results");
-			myAlert.setContentText("This circuit has a voltage of " + myCircuit.getVoltage());
-			myAlert.showAndWait();
+			while (!error) {
+				try {
+					myCircuit.setResistance(Double.parseDouble(resistanceTextField.getText()));
+					myCircuit.setAmperage(Double.parseDouble(amperageTextField.getText()));
+					if ((myCircuit.getAmperage() <= 0) || (myCircuit.getResistance() <= 0)) {
+						Alert myAlert = new Alert(AlertType.INFORMATION);
+						myAlert.setTitle("Error");
+						myAlert.setContentText("Please enter positive values.");
+						myAlert.showAndWait();
+						break;
+					}
+					myCircuit.calculateVoltage();
+					error = false;
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Results");
+					myAlert.setContentText("This circuit has an voltage of " + myCircuit.getVoltage());
+					myAlert.showAndWait();
+				} catch (Exception e) {
+					Alert myAlert = new Alert(AlertType.INFORMATION);
+					myAlert.setTitle("Error");
+					myAlert.setContentText("Please make sure that the values you entered are valid.");
+					myAlert.showAndWait();
+					break;
+				}
+			}
 		} else {
 			Alert myAlert = new Alert(AlertType.INFORMATION);
 			myAlert.setTitle("Error");
@@ -78,7 +138,7 @@ public class Controller {
 	}
 
 	@FXML
-	void handleClear(ActionEvent event) {
+	final void handleClear(final ActionEvent event) {
 		amperageTextField.clear();
 		resistanceTextField.clear();
 		voltageTextField.clear();
