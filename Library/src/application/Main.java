8c7de1efd15785b0feea41094cfc13905db0cf84
Caleb <214475@student.cofo.edu>
2020-03-07 13:56:55 -0600

    Eh.

--- Library/src/application/Main.java
+++ Library/src/application/Main.java
@@ -1,10 +1,12 @@
 package application;
 
+import controller.Controller;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * 
@@ -12,10 +14,16 @@ import javafx.scene.Scene;
  *
  */
 public class Main extends Application {
+	// static Controller myControllerHandle;
 	@Override
 	public final void start(final Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/view/View.fxml"));
+			// FXMLLoader loader = new
+			// FXMLLoader(getClass().getResource("/view/View.fxml"));
+			// Parent root = loader.load();
+			// myControllerHandle = (Controller) loader.getController();
+			// myControllerHandle.tcAuthor;
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
