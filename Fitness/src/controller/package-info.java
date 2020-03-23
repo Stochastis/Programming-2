218ded3b2502a9e4cb868f10c12e0100002697f1
Caleb <214475@student.cofo.edu>
2020-03-23 11:45:40 -0500

    Mar. 23, 2020 @ 11:45. End of class. Working.

---
+++ Fitness/src/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Caleb
+ *
+ */
+package controller;
---
+++ Fitness/src/module-info.java
@@ -0,0 +1,13 @@
+module fitness {
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires java.sql;
+	requires mysql.connector.java;
+
+	exports application to javafx.graphics;
+
+	opens controller to javafx.fxml;
+	opens model to javafx.base;
+}
