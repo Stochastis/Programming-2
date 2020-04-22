4ba1f0b7101fd6e679f795171cacfb1799e83b5c
Caleb <214475@student.cofo.edu>
2020-04-22 12:10:54 -0500

    Apr. 22, 2020 @ 12:10. Stable.

--- HelloInterwebs/WebContent/index.jsp
+++ HelloInterwebs/WebContent/index.jsp
@@ -10,25 +10,21 @@
 <link rel="stylesheet" href="style.css" />
 </head>
 <body>
+
 	<h1>This will calculate amperage given voltage and resistance are
 		known.</h1>
-	<form>
+	<form method=post>
 		<label for="txtVotlage">Voltage</label><input name="txtVoltage"
 			type="number" min="0.01" step="0.01" required /> <label
 			for="txtResistance">Resistance</label><input name="txtResistance"
 			type="number" min="0.01" step="0.01" required /> <input
 			type="submit" value="Calculate Amperage" name="cadSubmit" />
 
-		<%
-			if (request.getParameter("cadSubmit") != null) {
-				Circuit myCircuit = new Circuit();
 
-				myCircuit.setVoltage(Double.parseDouble(request.getParameter("txtVoltage")));
-				myCircuit.setResistance(Double.parseDouble(request.getParameter("txtResistance")));
-
-				out.println("The amperage is " + myCircuit.getAmperage());
-			}
-		%>
 	</form>
+	<%
+		Circuit myCircuit = (Circuit) request.getAttribute("myCircuit");
+		out.println("The amperage is " + myCircuit.getAmperage());
+	%>
 </body>
 </html>
---
+++ HelloInterwebs/src/controller/Controller.java
@@ -0,0 +1,53 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Circuit;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Controller() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Circuit myCircuit = new Circuit();
+
+		if (request.getParameter("cadSubmit") != null) {
+			myCircuit.setVoltage(Double.parseDouble(request.getParameter("txtVoltage")));
+			myCircuit.setResistance(Double.parseDouble(request.getParameter("txtResistance")));
+		}
+		request.setAttribute("myCircuit", myCircuit);
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
